f5befeb883e3b837134d6221f8aaff056f87126c
Alexia Ionescu <alexiaionescu99@gmail.com>
2021-03-30 11:31:02 +0300

    Seminar 6 - Prototype implementation

---
+++ Week6 - Builder Prototype Simple Factory/src/ro/ase/csie/cts/g1092/dp/prototype/TestPrototype.java
@@ -0,0 +1,18 @@
+package ro.ase.csie.cts.g1092.dp.prototype;
+
+public class TestPrototype {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+		Tree3DModel tree1 = new Tree3DModel("Green", 200);
+		//Tree3DModel tree2 = new Tree3DModel("Red", 150);
+		//Tree3DModel tree3 = new Tree3DModel("Green", 200);
+
+		//Tree3DModel tree4 = tree1; //shallow copy (you don't create a new object, you have another reference pointing to tree1)
+		
+		Tree3DModel tree5 = (Tree3DModel) tree1.clone();
+		Tree3DModel tree6 = (Tree3DModel) tree1.clone();
+		Tree3DModel tree7 = (Tree3DModel) tree1.clone();
+		
+		System.out.println("The end");
+	}
+}
